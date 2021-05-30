3501dcd21f84101a6d0dc5d33961610d206719fb
Murat Ilgun <muratilgun34@hotmail.com>
2021-05-30 15:18:13 +0300

    Add project files.

---
+++ DemoIdentity/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using DemoIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoIdentity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<DemoIdentityUser> userManager;
+
+        public HomeController(UserManager<DemoIdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByNameAsync(model.UserName);
+
+                if (user == null)
+                {
+                    user = new DemoIdentityUser
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        UserName = model.UserName
+                    };
+
+                    var result = await userManager.CreateAsync(user, model.Password);
+                }
+
+                return View("Success");
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ DemoIdentity/Models/DemoIdentityUserStore.cs
@@ -0,0 +1,145 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoIdentity.Models
+{
+    public class DemoIdentityUserStore : IUserStore<DemoIdentityUser>, IUserPasswordStore<DemoIdentityUser>
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public Task<string> GetUserIdAsync(DemoIdentityUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.Id);
+        }
+
+        public Task<string> GetUserNameAsync(DemoIdentityUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.UserName);
+
+        }
+
+        public Task SetUserNameAsync(DemoIdentityUser user, string userName, CancellationToken cancellationToken)
+        {
+            user.UserName = userName;
+            return Task.CompletedTask;
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(DemoIdentityUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.NormalizedUserName);
+        }
+
+        public Task SetNormalizedUserNameAsync(DemoIdentityUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+            user.NormalizedUserName = normalizedName;
+            return Task.CompletedTask;
+        }
+
+        public static DbConnection GetOpenConnection()
+        {
+            var connection = new SqlConnection("Data Source=(LocalDb)\\MSSQLLocalDB;" +
+                                   "database=IdentityDemo;" +
+                                   "trusted_connection=yes;");
+            connection.Open();
+
+            return connection;
+        }
+
+        public async Task<IdentityResult> CreateAsync(DemoIdentityUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                await connection.ExecuteAsync(
+                    "insert into IdentityDemoUsers([Id]," +
+                    "[UserName]," +
+                    "[NormalizedUserName]," +
+                    "[PasswordHash]) " +
+                    "Values(@id,@userName,@normalizedUserName,@passwordHash)",
+                    new
+                    {
+                        id = user.Id,
+                        userName = user.UserName,
+                        normalizedUserName = user.NormalizedUserName,
+                        passwordHash = user.PasswordHash
+                    }
+                );
+            }
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> UpdateAsync(DemoIdentityUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                await connection.ExecuteAsync(
+                    "update IdentityDemoUsers " +
+                    "set [Id] = @id," +
+                    "[UserName] = @userName," +
+                    "[NormalizedUserName] = @normalizedUserName," +
+                    "[PasswordHash] = @passwordHash " +
+                    "where [Id] = @id",
+                    new
+                    {
+                        id = user.Id,
+                        userName = user.UserName,
+                        normalizedUserName = user.NormalizedUserName,
+                        passwordHash = user.PasswordHash
+                    }
+                );
+            }
+
+            return IdentityResult.Success;
+        }
+
+        public Task<IdentityResult> DeleteAsync(DemoIdentityUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DemoIdentityUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                return await connection.QueryFirstOrDefaultAsync<DemoIdentityUser>(
+                    "select * From IdentityDemoUsers where Id = @id",
+                    new { id = userId });
+            }
+        }
+
+        public async Task<DemoIdentityUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                return await connection.QueryFirstOrDefaultAsync<DemoIdentityUser>(
+                    "select * From IdentityDemoUsers where NormalizedUserName = @name",
+                    new { name = normalizedUserName });
+            }
+        }
+
+        public Task SetPasswordHashAsync(DemoIdentityUser user, string passwordHash, CancellationToken cancellationToken)
+        {
+            user.PasswordHash = passwordHash;
+            return Task.CompletedTask;
+        }
+
+        public Task<string> GetPasswordHashAsync(DemoIdentityUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(DemoIdentityUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PasswordHash != null);
+        }
+    }
+}
